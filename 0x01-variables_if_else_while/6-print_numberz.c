896b6473cdc7c77051c4bedddc60d64e5c22f987
Hanim Yasin <haniyas3635@gmail.com>
2022-12-12 04:31:27 -0800

    numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,11 +1,19 @@
 #include <stdio.h>
+
 /**
- * main - Prints the numbers from 0-9.
-(*
- * Return: 0 on success
+ * main- Entry point
+ * @void: Null value
+ *
+ * Description: Print numbers from 0 to 9
+ * Return: Zero value
  */
+
 int main(void)
 {
-	printf("0123456789\n");
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+		putchar(n);
+	putchar('\n');
 	return (0);
 }
